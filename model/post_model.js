9af99295cf6be416453401f069b8a40b2e19b723
ch1p98 <alexpalladium@gmail.com>
2023-10-04 13:00:30 +0800

    add try catch block

--- model/post_model.js
+++ model/post_model.js
@@ -10,8 +10,13 @@ const getListOfPost = async (data) => {
   const { keyword } = data;
   const sql = `SELECT * FROM posts p JOIN post_tags ON p.post_id = post_tags.post_id 
     JOIN tags ON post_tags.tag_id = tags.tag_id WHERE tags.name = ?`;
-  const result = await db.execute(sql, [keyword]);
-  return result;
+
+  try {
+    const result = await db.execute(sql, [keyword]);
+    return result;
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = {
