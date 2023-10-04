5dee4de21251bab3d277fc4efc8ba80655bf6e01
ch1p98 <alexpalladium@gmail.com>
2023-10-04 13:03:09 +0800

    add debug statement

--- controllers/pages.js
+++ controllers/pages.js
@@ -4,8 +4,12 @@ const getHomePage = async (req, res) => {
   const { keyword } = req.query;
   let posts = {};
   let channel;
+  console.log(keyword);
+  console.log("keyword is false: " + !keyword);
   if (!keyword) {
     channel = "人生";
+  } else {
+    channel = keyword;
   }
   posts = await getListOfPost({ channel });
   res.send({ posts });
